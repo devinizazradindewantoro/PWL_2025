05a0811280dc8c1da4cc48aac762bb8f0ab85a3a
devinizazradindewantoro <darkdevin469@gmail.com>
2025-03-04 05:41:41 +0700

    Praktikum-1

--- Minggu_3/Jobsheet3/PWL_POS/routes/web.php
+++ Minggu_3/Jobsheet3/PWL_POS/routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
+//Route 
+Route::get('/welcome', function () {
     return view('welcome');
 });
