28b8f7f6c8d26d0adb1d26a07c40cad3e6168973
devinizazradindewantoro <darkdevin469@gmail.com>
2025-03-06 14:03:42 +0700

    Praktikum-1

--- Minggu_4/Jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_4/Jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,14 +10,25 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
 
-        // coba akses model UserModel
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
+
+        // tambah data user dengan Eloquent Model
+        //$data = [
+          //  'nama' => 'Pelanggan Pertama',
+        //];
+        //UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
     }
 }
