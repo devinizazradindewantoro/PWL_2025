b07d209604ef7661168cc451a156688f4828a758
devinizazradindewantoro <darkdevin469@gmail.com>
2025-03-16 13:37:32 +0700

    Praktikum 3 – Membuat form kemudian menyimpan data dalam database

--- Minggu_5/Jobsheet5/PWL_POS/resources/views/kategori/index.blade.php
+++ Minggu_5/Jobsheet5/PWL_POS/resources/views/kategori/index.blade.php
@@ -1,11 +1,11 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 {{-- Customize layout sections --}}
 
 @section('subtitle', 'Kategori')
 
 @section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Kagegori')
+@section('content_header_subtitle', 'Kategori')
 
 @section('content')
     <div class="container">
