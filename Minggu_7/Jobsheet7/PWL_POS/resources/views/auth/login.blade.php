bea4659dc22202214c7657aa11f3417c31acc68b
devinizazradindewantoro <darkdevin469@gmail.com>
2025-03-27 19:22:40 +0700

    Update POS

--- Minggu_7/Jobsheet7/PWL_POS/resources/views/auth/login.blade.php
+++ Minggu_7/Jobsheet7/PWL_POS/resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login Pengguna</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -17,6 +17,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
+
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="card card-outline card-primary">
@@ -58,12 +59,6 @@
           </div>
         </div>
       </form>
-
-      <!-- Register Link -->
-      <div class="mt-3 text-center">
-        <p>Don't have an account? <a href="{{ url('register') }}">Register here</a></p>
-      </div>
-      
     </div>
   </div>
 </div>
