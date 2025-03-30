ea3c170fdbd50505a07c564b82cd06c505cdb921
devinizazradindewantoro <darkdevin469@gmail.com>
2025-03-30 22:37:36 +0700

    Praktikum 3 – Implementasi Export PDF di Laravel dengan dompdf

--- Minggu_8/Jobsheet8/PWL_POS/routes/web.php
+++ Minggu_8/Jobsheet8/PWL_POS/routes/web.php
@@ -122,6 +122,7 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::get('/import', [BarangController::class, 'import']);                  // ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']);       // ajax import excel
             Route::get('/export_excel', [BarangController::class, 'export_excel']);          // export excel
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']);             // export pdf
             Route::delete('/{id}', [BarangController::class, 'destroy']);                       // menghapus data barang
         });
     });
