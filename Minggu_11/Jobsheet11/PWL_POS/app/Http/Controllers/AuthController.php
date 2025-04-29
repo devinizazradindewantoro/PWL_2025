f59538dc2b433cca067956057b785a8f235cfe8a
[devinizazradindewantoro] <[darkdevin469@gmail.com]>
2025-04-29 10:33:45 +0700

    Jobsheet 11

---
+++ Minggu_11/Jobsheet11/PWL_POS/app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\UserModel;
+use App\Models\LevelModel;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        if (Auth::check()) { // jika sudah login, maka redirect ke halaman home return redirect('/');
+        }
+        return view('auth.login');
+    }
+
+    public function postlogin(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $credentials = $request->only('username', 'password');
+
+            if (Auth::attempt($credentials)) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Login Berhasil',
+                    'redirect' => url('/')
+                ]);
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'Login Gagal'
+            ]);
+        }
+
+        return redirect('login');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
+    }
+
+    public function register()
+    {
+        $levels = LevelModel::all();
+        return view('auth.register', ['level' => $levels]);
+    }
+
+    public function postRegister(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100', 
+            'password' => 'required|min:5', 
+            'level_id' => 'required|integer', 
+        ]);
+    
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password), 
+            'level_id' => $request->level_id
+        ]);
+    
+        return redirect('login')->with('success', 'Registrasti berhasil ditambahkan');
+    }
+}
---
+++ Minggu_11/Jobsheet11/PWL_POS/routes/api.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+Route::get('category', [KategoriController::class, 'index']);
+Route::post('category', [KategoriController::class, 'store']);
+Route::get('category/{kategori}', [KategoriController::class, 'show']);
+Route::put('category/{kategori}', [KategoriController::class, 'update']);
+Route::delete('category/{kategori}', [KategoriController::class, 'destroy']);
+
+Route::get('items', [BarangController::class, 'index']);
+Route::post('items', [BarangController::class, 'store']);
+Route::get('items/{barang}', [BarangController::class, 'show']);
+Route::put('items/{barang}', [BarangController::class, 'update']);
+Route::delete('items/{barang}', [BarangController::class, 'destroy']);
